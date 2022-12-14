89054a79844f333fe3e32ee0e3ff303126fcff54
Przemyslaw Soszynski <psoszynski@gmail.com>
2022-12-14 19:18:03 +0100

    part 2 modifications

--- 2022/12/aoc2022_12/aoc2022_12/Program.cs
+++ 2022/12/aoc2022_12/aoc2022_12/Program.cs
@@ -1,50 +1,72 @@
 ﻿using Spectre.Console;
+using System.Diagnostics;
+
 AnsiConsole.ResetColors();
 
-Thread.Sleep(2);
-var input = File.ReadAllLines(@"C:\Users\psoszyns\programming\2022\12\input.txt");
+Thread.Sleep(20);
+var input = File.ReadAllLines(@"input.txt");
 var length = input[0].Length; //8
 #pragma warning disable CA1416 // Validate platform compatibility
 Console.BufferWidth = Console.LargestWindowWidth;
 Console.BufferHeight = Console.LargestWindowHeight;
 #pragma warning restore CA1416 // Validate platform compatibility
 
-var y = 0;
-foreach (var line in input)
-{
-    var x = 0;
-    foreach (var c in line)
-    {
-        int c1 = Convert.ToInt32(22 * ((float)c - 97) / 25) + 232;
-        Console.CursorLeft = x;
-        Console.CursorTop = y;
-        AnsiConsole.Background = Spectre.Console.Color.FromInt32(c1);
-        AnsiConsole.Write(c);
-        x++;
-    }
-    y++;
-}
+//PrintStartMap(input);
 
-(var start, var stop, var rows) = FindStartStop(input);
+(var allAPoints, var start, var rows) = FindAllAPoints(input);
 
-var points = new HashSet<(int, int)>();
-var newPoints = new HashSet<(int, int)>();
-var visitedPoints = new HashSet<(int, int)>();
+HashSet<(int, int)> points;
+HashSet<(int, int)> newPoints;
+HashSet<(int, int)> visitedPoints;
 
-points.Add((start));
 var i = 1;
-while (true)
-{
-    (var foundDestination, newPoints) = NextIteration(points);
-    PrintPoints(newPoints, Spectre.Console.Color.Red);
-    Thread.Sleep(20);
-    PrintPoints(points);
-    if (foundDestination || newPoints.Count() == 0) break;
-    points = newPoints;
-    i++;
-}
 
-Console.CursorTop = input.Count()-1;
+//part 1
+
+//points = new HashSet<(int, int)>();
+//newPoints = new HashSet<(int, int)>();
+//visitedPoints = new HashSet<(int, int)>();
+//points.Add(start);
+//while (true)
+//{
+//    (var foundDestination, newPoints) = NextIteration(points);
+//    PrintPoints(newPoints, Spectre.Console.Color.Red);
+//    Thread.Sleep(20);
+//    PrintPoints(points);
+//    if (foundDestination || newPoints.Count() == 0) break;
+//    points = newPoints;
+//    i++;
+//}
+//Console.CursorTop = input.Count() - 1;
+//Console.WriteLine(++i);
+
+//part 2
+allAPoints.Add(start);
+var shortestWay = 1000;
+foreach (var a in allAPoints)
+{
+    points = new HashSet<(int, int)>();
+    newPoints = new HashSet<(int, int)>();
+    visitedPoints = new HashSet<(int, int)>();
+
+    points.Add(a);
+    var foundDestination = false;
+    i = 1;
+    while (true)
+    {
+        (foundDestination, newPoints) = NextIteration(points);
+        //PrintPoints(newPoints, Spectre.Console.Color.Red);
+        //Thread.Sleep(20);
+        //PrintPoints(points);
+        if (foundDestination || newPoints.Count() == 0) break;
+        points = newPoints;
+        i++;
+    }
+
+    if (foundDestination && ++i < shortestWay) shortestWay = i;
+    Console.WriteLine(shortestWay);
+}
+Console.CursorTop = input.Count() - 1;
 
 (bool, HashSet<(int, int)>) NextIteration(HashSet<(int, int)> points)
 {
@@ -113,30 +135,25 @@ Console.CursorTop = input.Count()-1;
     return (false, newPoints);
 }
 
-((int, int), (int, int), int) FindStartStop(string[] input)
+(HashSet<(int, int)>, (int, int), int) FindAllAPoints(string[] input)
 {
     var row = 0;
+    var allAPoints = new HashSet<(int, int)>();
     (int x, int y) start = (-1, -1);
     (int x, int y) end = (-1, -1);
     foreach (var l in input)
     {
-        var lChar = l.ToCharArray();
-        var sElement = lChar.Select((x, i) => (x, i)).Where(i => i.Item1 == 'S');
-        if (sElement.Count() > 0)
+        for (var i = 0; i < l.Length; i++)
         {
-            start = (sElement.First().Item2, row);
-        }
-        var eElement = lChar.Select((x, i) => (x, i)).Where(i => i.Item1 == 'E');
-        if (eElement.Count() > 0)
-        {
-            end = (eElement.First().Item2, row);
+            if (l[i] == 'S') start = (i, row);
+            if (l[i] == 'a') allAPoints.Add((i, row));
         }
         row++;
     }
-    return (start, end, row);
+    return (allAPoints, start, row);
 }
 
-void PrintPoints(HashSet<(int,int)> points, Spectre.Console.Color? color = null)
+void PrintPoints(HashSet<(int, int)> points, Spectre.Console.Color? color = null)
 {
     if (color.HasValue)
     {
@@ -145,7 +162,7 @@ void PrintPoints(HashSet<(int,int)> points, Spectre.Console.Color? color = null)
             Console.CursorLeft = p.Item1;
             Console.CursorTop = p.Item2;
             AnsiConsole.Background = color.Value;
-            AnsiConsole.Write(input[p.Item2][p.Item1]);
+            AnsiConsole.Write(input![p.Item2][p.Item1]);
         }
     }
     else
@@ -154,9 +171,28 @@ void PrintPoints(HashSet<(int,int)> points, Spectre.Console.Color? color = null)
         {
             Console.CursorLeft = p.Item1;
             Console.CursorTop = p.Item2;
-            var c = input[p.Item2][p.Item1];
+            var c = input![p.Item2][p.Item1];
             AnsiConsole.Background = Convert.ToInt32(22 * ((float)c - 97) / 25) + 232;
             AnsiConsole.Write(input[p.Item2][p.Item1]);
         }
+    }
+}
+
+static void PrintStartMap(string[] input)
+{
+    var y = 0;
+    foreach (var line in input)
+    {
+        var x = 0;
+        foreach (var c in line)
+        {
+            int c1 = Convert.ToInt32(22 * ((float)c - 97) / 25) + 232;
+            Console.CursorLeft = x;
+            Console.CursorTop = y;
+            AnsiConsole.Background = Spectre.Console.Color.FromInt32(c1);
+            AnsiConsole.Write(c);
+            x++;
+        }
+        y++;
     }
 }
