c2715b46202f8cfa12f6fb944dd90d7e09f6dda7
Przemyslaw Soszynski <psoszynski@gmail.com>
2024-12-02 06:39:02 +0100

    2024 has begun

---
+++ 2024/01/Program.cs
@@ -0,0 +1,38 @@
+﻿
+//var input = File.ReadAllLines("testinput.txt");
+var input = File.ReadAllLines("input.txt");
+
+var fc = new List<int>();
+var sc = new List<int>();
+foreach (var line in input)
+{
+    fc.Add(int.Parse(line.Split("   ")[0]));
+    sc.Add(int.Parse(line.Split("   ")[1]));
+}
+
+var fcsorted = fc.OrderBy(x => x).ToList();
+var scsorted = sc.OrderBy(x => x).ToList();
+
+
+var sum = 0;
+for (int i = 0; i < fcsorted.Count; i++)
+{
+    var currentDifference = Math.Abs(fcsorted[i] - scsorted[i]);
+    sum += currentDifference;
+    //Console.WriteLine($"{fc[i]},{sc[i]}, {Math.Abs(fc[i] - sc[i])}");
+}
+
+Console.WriteLine(sum);
+
+//part 2
+
+sum = 0;
+for (int i = 0; i < fcsorted.Count; i++)
+{
+    var c = sc.Count(x => x == fc[i]);
+    var m = fc[i] * c;
+    sum += m;
+    //Console.WriteLine($"{fc[i]}, {c}, {m}");
+}
+
+Console.WriteLine(sum);
